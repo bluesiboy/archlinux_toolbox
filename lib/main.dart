954d555458e86f4873b960d41f661ec3e95033ba
blue <2855892566@qq.com>
2024-06-03 20:03:48 +0800

    12

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,79 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
 
-void main() {
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+
+int id = 0;
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+final StreamController<String?> selectNotificationStream = StreamController<String?>.broadcast();
+const String navigationActionId = 'id_3';
+
+@pragma('vm:entry-point')
+void notificationTapBackground(NotificationResponse notificationResponse) {
+  // ignore: avoid_print
+  print('notification(${notificationResponse.id}) action tapped: '
+      '${notificationResponse.actionId} with'
+      ' payload: ${notificationResponse.payload}');
+  if (notificationResponse.input?.isNotEmpty ?? false) {
+    // ignore: avoid_print
+    print('notification action tapped with input: ${notificationResponse.input}');
+  }
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final LinuxInitializationSettings initializationSettingsLinux = LinuxInitializationSettings(
+    defaultActionName: 'Open notification',
+    defaultIcon: AssetsLinuxIcon('icons/app_icon.png'),
+  );
+  final InitializationSettings initializationSettings = InitializationSettings(linux: initializationSettingsLinux);
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+    onDidReceiveNotificationResponse: (NotificationResponse notificationResponse) {
+      switch (notificationResponse.notificationResponseType) {
+        case NotificationResponseType.selectedNotification:
+          selectNotificationStream.add(notificationResponse.payload);
+          break;
+        case NotificationResponseType.selectedNotificationAction:
+          if (notificationResponse.actionId == navigationActionId) {
+            selectNotificationStream.add(notificationResponse.payload);
+          }
+          break;
+      }
+    },
+    onDidReceiveBackgroundNotificationResponse: notificationTapBackground,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      navigatorObservers: [FlutterSmartDialog.observer],
+      builder: FlutterSmartDialog.init(),
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      themeMode: ThemeMode.system,
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -56,70 +81,151 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
+    _showNotification(title: _counter.toString(), body: _counter.toString());
+    SmartDialog.showToast('$_counter');
+  }
+
+  Future<void> _showNotification({String title = 'plain titleplain title', String body = 'plain body'}) async {
+    const AndroidNotificationDetails androidNotificationDetails = AndroidNotificationDetails(
+        'your channel id', 'your channel name',
+        channelDescription: 'your channel description',
+        importance: Importance.max,
+        priority: Priority.high,
+        ticker: 'ticker');
+    const NotificationDetails notificationDetails = NotificationDetails(android: androidNotificationDetails);
+    await flutterLocalNotificationsPlugin.show(id++, title, body, notificationDetails, payload: 'item x');
+  }
+
+  @override
+  void initState() {
+    isLoadNBD();
+    super.initState();
+  }
+
+  var is_nbd_module_load = false;
+  Future<void> isLoadNBD() async {
+    var result = await runLinuxScript('ls', ['/dev/nbd*']);
+    is_nbd_module_load = result == 0;
+    setState(() {});
+  }
+
+  Future<void> loadNBDModule() async {
+    var result = await runLinuxScript('pkexec', ['modprobe', 'nbd']);
+    setState(() {});
+  }
+
+  List<String> prints = [];
+  var vd1 = TextEditingController();
+  bool isMounting = false;
+
+  Future<int> runLinuxScript(String scriptPath, List<String> arguments) async {
+    // prints = ['$scriptPath ${arguments.join(' ')}'];
+    prints = ['/usr/bin/bash -c \'$scriptPath ${arguments.join(' ')}\''];
+    // 使用ProcessStart信息来配置脚本执行
+    // final process = await Process.start(scriptPath, arguments, runInShell: true);
+    final process = await Process.start('/bin/sh', ['-c', '\'$scriptPath ${arguments.join(' ')}\'']);
+
+    // 监听脚本的stdout
+    process.stdout.transform(Utf8Decoder()).listen((ob_) {
+      prints.add(ob_);
+    });
+
+    // 监听脚本的stderr
+    process.stderr.transform(Utf8Decoder()).listen((ob_) {
+      prints.add(ob_);
+    });
+    // 等待脚本执行完成
+    var result = await process.exitCode;
+    prints.add('exitCode: $result');
+    return result;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          body: Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      if (is_nbd_module_load)
+                        ListTile(
+                          leading: const Icon(Icons.check_circle_sharp, color: Colors.green),
+                          title: const Text('The nbd module is loaded'),
+                          trailing: TextButton(
+                            onPressed: () async {
+                              var result = await runLinuxScript('modprobe', ['-r', 'nbd']);
+                              setState(() {});
+                            },
+                            child: const Text('unload'),
+                          ),
+                        ),
+                      if (!is_nbd_module_load)
+                        ListTile(
+                          leading: const Icon(Icons.warning, color: Colors.amber),
+                          title: const Text('The nbd module is not loaded'),
+                          trailing: TextButton(onPressed: loadNBDModule, child: const Text('load')),
+                        ),
+                      TextField(
+                        controller: vd1,
+                        decoration: InputDecoration(
+                            hintText: ('disk file'),
+                            labelText: '/dev/nbd0',
+                            helperText: 'qemu-nbd -c /dev/nbdX /path/to/file',
+                            suffix: isMounting
+                                ? const CircularProgressIndicator()
+                                : TextButton(
+                                    onPressed: () async {
+                                      setState(() {
+                                        isMounting = true;
+                                      });
+                                      SmartDialog.showLoading(msg: '挂载中...');
+                                      var file = vd1.text;
+                                      if (File(file).existsSync()) {
+                                        await runLinuxScript('qemu-nbd', ['-c', '/dev/nbd0', file]);
+                                        await SmartDialog.dismiss();
+                                      } else {
+                                        await SmartDialog.dismiss();
+                                        await SmartDialog.showNotify(msg: '文件不存在！', notifyType: NotifyType.failure);
+                                      }
+                                      setState(() {
+                                        isMounting = false;
+                                      });
+                                    },
+                                    child: const Text('mount'))),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  height: 180,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: prints.map((e) => Text(e)).cast<Text>().toList(),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
